61265f25c1e2f29115983b3f2171a866fd8fe248
zkaakakag <cscom222@naver.com>
2025-02-26 11:21:56 +0900

    db 연결

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 // 데이터베이스 연결 설정
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "192.168.1.65",
   user: "ninja",
   password: "1234",
   database: "bufl",
@@ -12,12 +12,6 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
   console.log("Connected to the database!");
-});
-
-// 쿼리 실행
-connection.query("SELECT * FROM Users", (err, results) => {
-  if (err) throw err;
-  console.log(results);
 });
 
 // 연결 종료
