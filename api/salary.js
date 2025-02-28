ffb37f7df9ab59beaae16389bdd0f0ac8b2f85f8
zkaakakag <cscom222@naver.com>
2025-02-28 10:14:40 +0900

    카테고리 추가 구현

--- api/salary.js
+++ api/salary.js
@@ -15,8 +15,22 @@ router.get("/", async (req, res) => {
 router.post("/", (req, res) => {});
 
 router.post("/category", async (req, res) => {
-  const [category_name, target_amount, start_date, color] = req.body;
+  const { name, goal_amount, start_date, background_color } = req.body;
 
   try {
-  } catch (err) {}
+    if (!name || !start_date || !background_color) {
+      return res.status(400).json({ message: "모든 정보를 입력하세요" });
+    }
+    const [result] = await db.query(
+      "INSERT INTO Categories (name, goal_amount, start_date, background_color) VALUES (?, ?, ?, ?)",
+      [name, goal_amount, start_date, background_color]
+    );
+
+    res.status(201).json({ message: "카테고리 추가 성공" });
+  } catch (err) {
+    console.error("카테고리 추가 오류:", err);
+    res.status(500).json({ message: "서버 오류" });
+  }
 });
+
+module.exports = router;
